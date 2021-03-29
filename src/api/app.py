e9dc20c87839c08066022357a2f5fe2ba1d0f51c
haydenjeune <33794706+haydenjeune@users.noreply.github.com>
2021-03-30 10:51:26 +1300

    Accept post with image data

--- src/api/app.py
+++ src/api/app.py
@@ -1,12 +1,17 @@
 import connexion
+from PIL import Image
+from io import BytesIO
 
 
 def ping():
     return "pong"
 
-    import connexion
+
+def find_lookalike(body: bytes):
+    im = Image.open(BytesIO(body)).convert("RGB")
+    return {"celeb": "test"}, 200
 
 
 app = connexion.FlaskApp(__name__)
-app.add_api("spec.yaml")
+app.add_api("spec.yaml", strict_validation=True)
 app.run(port=8080)
