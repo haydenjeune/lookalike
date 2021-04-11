b2de869fb10437e2676ea2cd0a0b0beaa2e87886
haydenjeune <33794706+haydenjeune@users.noreply.github.com>
2021-04-11 16:16:33 +1200

    Add basic picture taking page

--- web/lookalike-web/src/App.tsx
+++ web/lookalike-web/src/App.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Webcam from 'react-webcam';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import ReplayIcon from '@material-ui/icons/Replay';
+
+const CapturedPicture = () => {
+
+}
+
+const WebcamCapture = () => {
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState<string>("");
+  const [photoTaken, setPhotoTaken] = React.useState<boolean>(false);
+
+  const capture = React.useCallback(() => {
+    if (webcamRef.current) {
+      // TODO: figure out how to do this properly
+      const imageSrc = (webcamRef.current! as any).getScreenshot();
+      setImgSrc(imageSrc);
+    }
+  }, [webcamRef, setImgSrc]);
+
+  if (imgSrc !== "") {
+    return (
+      <>
+        <img src={imgSrc} />
+        <IconButton color="primary" aria-label="retake picture" component="span" onClick={()=>{setImgSrc("")}}>
+          <ReplayIcon fontSize="large"/>
+        </IconButton>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{facingMode: "user"}}
+        mirrored={true}
+      />
+      <IconButton color="primary" aria-label="take picture" component="span" onClick={capture}>
+          <PhotoCamera fontSize="large"/>
+      </IconButton>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WebcamCapture />
     </div>
   );
 }
