9736785438727fdc25fc6606e6744bc20be6ce1e
haydenjeune <33794706+haydenjeune@users.noreply.github.com>
2021-04-25 13:25:38 +1200

    Return 400 on bad input

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,11 @@
 from typing import NamedTuple
-import connexion
-from PIL import Image
+from connexion import FlaskApp, ProblemException
+from PIL import Image, UnidentifiedImageError
 from io import BytesIO
 from pathlib import Path
 from flask_cors import CORS
 import base64
+from binascii import Error as DecodeError
 
 from api import configuration
 from index.predictor import FaceNetDotProductPredictor
@@ -18,16 +19,29 @@ def ping():
     return "pong"
 
 
+def _deserialise_image(data: bytes) -> Image.Image:
+    try:
+        data = base64.b64decode(data)
+        im = Image.open(BytesIO(data)).convert("RGB")
+        return im
+    except DecodeError as e:
+        raise ProblemException(
+            400, "Bad encoding", "Image data must be base64 encoded"
+        ) from e
+    except UnidentifiedImageError as e:
+        raise ProblemException(400, "Bad data", "Decoded data was not an image") from e
+
+
 def find_lookalike(body: bytes):
-    body = base64.b64decode(body)
-    im = Image.open(BytesIO(body)).convert("RGB")
+    im = _deserialise_image(body)
     celebs = predictor.predict(im)
+
     return celebs, 200
 
 
-connexion_app = connexion.FlaskApp(__name__, specification_dir=Path(__file__).parent)
+connexion_app = FlaskApp(__name__, specification_dir=Path(__file__).parent)
 connexion_app.add_api("spec.yaml", strict_validation=True)
-connexion_app.add_error_handler(Exception, connexion.FlaskApp.common_error_handler)
+connexion_app.add_error_handler(Exception, FlaskApp.common_error_handler)
 CORS(connexion_app.app)  # TODO: only do this in dev for all origins
 
 if __name__ == "__main__":
---
+++ tests/unit/test_api.py
@@ -0,0 +1,36 @@
+import pytest
+import api.app as app
+from connexion import ProblemException
+
+
+@pytest.fixture()
+def b64_jpg_data():
+    return b"/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD5/ooooA//2Q=="
+
+
+@pytest.fixture()
+def b64_png_data():
+    return b"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII="
+
+
+@pytest.fixture()
+def non_b64_png_data():
+    return b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x01\x03\x00\x00\x00%\xdbV\xca\x00\x00\x00\x03PLTE\x00\x00\x00\xa7z=\xda\x00\x00\x00\x01tRNS\x00@\xe6\xd8f\x00\x00\x00\nIDAT\x08\xd7c`\x00\x00\x00\x02\x00\x01\xe2!\xbc3\x00\x00\x00\x00IEND\xaeB`\x82"
+
+
+@pytest.fixture()
+def b64_non_image_data():
+    return b"YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo="
+
+
+def test_post_throws_exception_with_unencoded_data(non_b64_png_data):
+    with pytest.raises(ProblemException):
+        app.find_lookalike(non_b64_png_data)
+
+
+def test_post_throws_exception_with_encoded_non_image_data(b64_non_image_data):
+    with pytest.raises(ProblemException):
+        app.find_lookalike(b64_non_image_data)
+
+
+
