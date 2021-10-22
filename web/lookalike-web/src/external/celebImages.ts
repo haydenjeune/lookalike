9b8b171759b8c0f13097da65298a511eec200605
haydenjeune <33794706+haydenjeune@users.noreply.github.com>
2021-10-23 11:59:28 +1300

    Include static images in monolithic docker file

--- web/lookalike-web/src/external/celebImages.ts
+++ web/lookalike-web/src/external/celebImages.ts
@@ -1,10 +1,5 @@
 export function getImageSrc(celebName: string) {
-  //  return encodeURI(
-  //    "https://lookalike-manual.s3-ap-southeast-2.amazonaws.com/" +
-  //      celebName +
-  //      "/0.jpg"
-  //  );
   return encodeURI(
-    "http://localhost:5678/" + celebName.replace(/[^\w]|_/g, "") + "/main.jpeg"
+    "http://localhost:8081/" + celebName.replace(/[^\p{L}]/ug, "") + "/main.jpeg"
   );
 }
